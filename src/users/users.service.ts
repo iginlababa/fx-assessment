0e7fe1bba5c4fce92a8cf838d3ef7d199b45266f
iginlababa <iginlababa8080@gmail.com>
2026-03-17 02:18:00 +0100

    feat: implement users service with create and lookup methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,13 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 
+interface CreateUserData {
+  email: string;
+  passwordHash: string;
+  firstName: string;
+  lastName: string;
+}
+
 @Injectable()
 export class UsersService {
   constructor(
@@ -10,11 +17,28 @@ export class UsersService {
     private readonly usersRepository: Repository<User>,
   ) {}
 
+  async createUser(data: CreateUserData): Promise<User> {
+    const user = this.usersRepository.create({
+      email: data.email,
+      password_hash: data.passwordHash,
+      first_name: data.firstName,
+      last_name: data.lastName,
+    });
+    return this.usersRepository.save(user);
+  }
+
   findByEmail(email: string): Promise<User | null> {
-    return this.usersRepository.findOne({ where: { email } });
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .where('LOWER(user.email) = LOWER(:email)', { email })
+      .getOne();
   }
 
   findById(id: string): Promise<User | null> {
     return this.usersRepository.findOne({ where: { id } });
+  }
+
+  async markEmailVerified(userId: string): Promise<void> {
+    await this.usersRepository.update(userId, { is_email_verified: true });
   }
 }
